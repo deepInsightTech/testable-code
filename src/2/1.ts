84f4c7a9a013a35e20a3c8fa4c50502a23499204
Mahesh Masalkar <mahi@Apples-MacBook-Pro.local>
2021-05-06 10:03:27 +0530

    Created a basic library to learn define io-ts types from APIs'

--- src/2/1.ts
+++
@@ -1,9 +0,0 @@
-export function helloWorld(name: string) {
-  return () => {  // IO operation which returns void
-    console.log(`Hello World: ${name}`)
-  }
-}
-
-const program = helloWorld('Mahi')
-
-program()
